5f7b174e8e405500b467f1592017dcd828a60b56
md rasif <mdrasif3499@gmail.com>
2024-11-27 18:00:24 +0600

    create Destination part

--- src/components/layouts/Destinations/Destinations.jsx
+++ src/components/layouts/Destinations/Destinations.jsx
@@ -65,6 +65,27 @@ const Destinations = () => {
           </div>
         </div>
       </div>
+
+
+      <div>
+        <div>
+        <div class="flex flex-col items-start p-6 bg-white  rounded-lg  max-w-sm">
+        <span class="text-sm font-bold uppercase text-red-500 tracking-wider mb-2">Destination List</span>
+        <h1 class="text-2xl font-bold text-gray-800 mb-4">
+          Explore the Beautiful Places Around World
+        </h1>
+        <p class="text-sm text-gray-500 mb-6">
+          Flexible Classes refers to the process of acquiring is knowledge free
+        </p>
+        <button class="px-4 py-2 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700 transition duration-200">
+          Discover More
+        </button>
+      </div>
+        </div>
+        
+
+
+      </div>
     </div>
   );
 };
