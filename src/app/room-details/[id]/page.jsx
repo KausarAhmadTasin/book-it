efe5cdbe3a993ba3deb9c16eff59fbb56550e374
alamin6688 <alamin.kzs06@gmail.com>
2024-11-29 01:32:12 +0600

    Room Details paage added

--- src/app/room-details/[id]/page.jsx
+++ src/app/room-details/[id]/page.jsx
@@ -1,13 +1,10 @@
-"use client";
-import Image from "next/image";
 import { FaLocationDot } from "react-icons/fa6";
-import { MdOutlineStarPurple500 } from "react-icons/md";
-import { MdAccessTimeFilled } from "react-icons/md";
-import { BsFillInfoCircleFill } from "react-icons/bs";
-import Link from "next/link";
+import { BsHighlights } from "react-icons/bs";
+import Image from "next/image";
 
-const FeaturedRooms = () => {
-  // Custom Room Data
+const Details = async ({ params }) => {
+  const { id } = await params;
+
   const roomsData = [
     {
       _id: "01",
@@ -220,101 +217,107 @@ const FeaturedRooms = () => {
     },
   ];
 
-  return (
-    <section className="pb-16 bg-base-200">
-      {/* Featured Rooms Title Section */}
-      <div className="py-8">
-        <h3 className="text-3xl md:text-2xl text-center text-[#083247] font-semibold pb-4">
-          Featured Rooms
-        </h3>
-        <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl text-center font-semibold text-[#333335] capitalize pb-0 md:pb-4">
-          Most Favourite Choices
-        </h1>
-      </div>
+  const currentRoom = roomsData.find((room) => room._id === id);
+  console.log(currentRoom);
 
-      {/* Cards */}
-      <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center container mx-auto px-4">
-        {roomsData.map((room) => (
-          <div key={room._id} className="shadow-2xl">
-            <div className="w-full shadow-lg rounded-xl">
-              <div className="flex w-full justify-between items-center px-4 py-6">
-                <div className="flex items-center gap-4">
-                  <div>
-                    <h2 className="font-semibold text-[#231F1C] tracking-wide text-2xl">
-                      {room.hotelName}
-                    </h2>
-                  </div>
-                </div>
-              </div>
-              <div className="relative overflow-hidden group cursor-pointer rounded-md">
+  return (
+    <section className="bg-[#EEF1F6] flex items-center justify-center min-h-screen">
+      <div className="container mx-auto text-[#333335] pt-10">
+        <div>
+          <div className="flex flex-col gap-6">
+            <div>
+              <div className="w-full h-[60vh] overflow-hidden rounded-xl">
                 <Image
-                  src={room.image_url}
-                  alt={room.hotelName}
-                  className="w-full h-[350px] object-cover group-hover:scale-[1.1] transition-all duration-500"
+                  className="w-full h-full object-cover"
+                  src={currentRoom.image_url}
+                  alt="header image"
                   width={400}
-                  height={350}
+                  height={400}
                 />
               </div>
-              <div className="p-8 text-[#083247]">
-                <div className="bg-[#E9F3F4] hover:bg-[#083247] transition-all duration-500 hover:text-white ease-out hover:scale-105 hover:transition-all hover:duration-500  mx-auto rounded-lg py-2">
-                  <div className="flex gap-2 items-center justify-start py-2 px-6">
-                    <MdOutlineStarPurple500 className="text-lg" />
-                    <p>
-                      {room.hotelRating} ({room.hotelReviews} reviews)
-                    </p>
-                  </div>
-                  <div className="flex gap-2 items-center justify-start py-2 px-6">
-                    <FaLocationDot className="mb-1" />
-                    <p>{`${room.address.street}, ${room.address.city}, ${room.address.country}`}</p>
-                  </div>
-                  <hr />
-                  <div className="flex items-center justify-between">
-                    <div className="flex gap-2 items-center justify-start py-2 px-6">
-                      <MdAccessTimeFilled />
-                      <p>3 Nights</p>
-                    </div>
-                    <div className="py-2 px-6 text-2xl font-bold">
-                      <p>${room.price}</p>
-                    </div>
-                  </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 pt-10 pb-16">
+            {/* Room Info */}
+            <div className="col-span-4 flex flex-col gap-4">
+              <div className="flex flex-col gap-2 text-lg font-semibold">
+                <div className="flex gap-2 items-center justify-start pb-2">
+                  <FaLocationDot className="mb-1 text-[#083248]" />
+                  <p>{`${currentRoom.address.street}, ${currentRoom.address.city}, ${currentRoom.address.country}`}</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <BsHighlights className="text-[#083248]" />
+                  <p>Room Highlights</p>
+                </div>
+                <div className="text-xl font-semibold flex flex-row items-center gap-2"></div>
+                <div className="grid grid-cols-2 md:grid-cols-3  items-center font-normal gap-4 tracking-wide">
+                  {currentRoom.amenities.map((amenity, index) => (
+                    <span
+                      key={index}
+                      className="inline-block bg-blue-100 text-blue-800 text-sm px-2 py-3 text-center rounded-full"
+                    >
+                      {amenity}
+                    </span>
+                  ))}
                 </div>
               </div>
 
-              {/* Status and Details Button */}
-              <div className="flex items-center justify-between w-full pt-0 pb-6 px-8">
-                <div className="flex items-center gap-2 bg-[#E4EEEF] p-4 rounded-full text-[#083248] hover:bg-[#083247] transition-all duration-500 hover:text-white ease-out hover:scale-105 hover:transition-all hover:duration-500">
-                  <BsFillInfoCircleFill className="text-xl" />
-                  <p className="text-sm font-bold capitalize tracking-wide">
-                    {room.status}
-                  </p>
+              <hr />
+              <div className="text-lg font-semibold space-y-2">
+                <p>Description</p>
+                <p className="font-normal">{currentRoom.description}</p>
+              </div>
+              <div className="text-lg font-semibold space-y-2">
+                <p>Cancellation Policy</p>
+                <p className="font-normal">{currentRoom.cancellationPolicy}</p>
+              </div>
+              <hr />
+            </div>
+
+            {/* Reservetion */}
+            <div className="md:col-span-3 order-first md:order-last pb-10">
+              <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-[#E9F3F4] p-4 space-y-4 shadow-xl">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="capitalize text-xl font-semibold">
+                    Status: {currentRoom.status}
+                  </div>
+                  <div className="text-2xl font-semibold">
+                    ${currentRoom?.price}{" "}
+                    <span className="font-normal">/night</span>
+                  </div>
+                </div>{" "}
+                <hr />
+                <div className="space-y-4 tracking-wide">
+                  <div className="flex flex-col md:flex-row items-center justify-between gap-2 font-semibold">
+                    <p>
+                      Max Occupancy:{" "}
+                      <span className="font-normal">
+                        {currentRoom.maxOccupancy}
+                      </span>
+                    </p>
+                    <p>
+                      BedType :{" "}
+                      <span className="font-normal">{currentRoom.bedType}</span>
+                    </p>
+                  </div>
+                  <div className="flex flex-col md:flex-row items-center justify-between gap-2 font-semibold">
+                    <p className="font-normal">{currentRoom.size}</p>
+                    <p className="font-normal">{currentRoom.view}</p>
+                  </div>
                 </div>
-                <div>
-                  <Link href={`/room-details/${room._id}`}>
-                    <button
-                      type="submit"
-                      className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-[#E4EEEF] backdrop-blur-md lg:font-semibold isolation-auto border-[#E4EEEF] before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-[#083248] hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
-                    >
-                      {/* Explore */}
-                      <svg
-                        className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
-                        viewBox="0 0 16 19"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
-                          className="fill-gray-800 group-hover:fill-gray-800"
-                        ></path>
-                      </svg>
-                    </button>
-                  </Link>
+                <hr />
+                <div className="p-4">
+                  <button class="w-full relative px-8 py-2 rounded-xl  bg-[#083248] isolation-auto z-10 border-2 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-[#2E8D9F] before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 text-white">
+                    Reserve
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      </div>{" "}
     </section>
   );
 };
 
-export default FeaturedRooms;
+export default Details;
